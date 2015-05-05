14131593dd7e4369ed0b2b46b02c9d1231ee99f4
agus4171 <agus.4171.wirawan@gmail.com>
2015-05-06 03:21:25 +0700

    update csr with form

---
+++ CA/ca-signed.php
@@ -0,0 +1,81 @@
+<?php
+include('File/X509.php');
+include('Crypt/RSA.php');
+
+// create private key for CA cert
+$CAPrivKey = new Crypt_RSA();
+extract($CAPrivKey->createKey());
+$CAPrivKey->loadKey($privatekey);
+
+$pubKey = new Crypt_RSA();
+$pubKey->loadKey($publickey);
+$pubKey->setPublicKey();
+
+echo "the private key for the CA cert (can be discarded):\r\n\r\n";
+echo $privatekey;
+echo "\r\n\r\n";
+
+
+
+
+
+
+
+// create a self-signed cert that'll serve as the CA
+$subject = new File_X509();
+$subject->setDNProp('id-at-countryName', 'ID');
+$subject->setDNProp('id-at-stateOrProvinceName', 'East Java');
+$subject->setDNProp('id-at-localityName', 'Surabaya');
+$subject->setDNProp('id-at-organizationName', 'sampi ningkang CA');
+$subject->setDNProp('id-at-organizationalUnitName', 'sampi ningkang signer');
+$subject->setDNProp('id-at-commonName', 'www.sampi-ningkang.com');
+$subject->setDNProp('id-at-emailAddress', 'mail@sampi-ningkang.com');
+$subject->setPublicKey($pubKey);
+
+$issuer = new File_X509();
+$issuer->setPrivateKey($CAPrivKey);
+$issuer->setDN($CASubject = $subject->getDN());
+
+$x509 = new File_X509();
+$x509->makeCA();
+$x509->setStartDate('-1 month');
+$x509->setEndDate('+1 year');
+
+$result = $x509->sign($issuer, $subject);
+echo "the CA cert to be imported into the browser is as follows:\r\n\r\n";
+echo $x509->saveX509($result);
+echo "\r\n\r\n";
+
+
+
+
+
+
+
+
+// create private key / x.509 cert for stunnel / website
+$privKey = new Crypt_RSA();
+extract($privKey->createKey());
+$privKey->loadKey($privatekey);
+
+$pubKey = new Crypt_RSA();
+$pubKey->loadKey($publickey);
+$pubKey->setPublicKey();
+
+$subject = new File_X509();
+$subject->setDNProp('id-at-organizationName', 'phpseclib demo cert');
+
+$subject->setPublicKey($pubKey);
+
+$issuer = new File_X509();
+$issuer->setPrivateKey($CAPrivKey);
+$issuer->setDN($CASubject);
+
+$x509 = new File_X509();
+$result = $x509->sign($issuer, $subject);
+echo "the stunnel.pem contents are as follows:\r\n\r\n";
+echo $privKey->getPrivateKey();
+echo "\r\n";
+echo $x509->saveX509($result);
+echo "\r\n";
+?>
--- CA/login.php
+++ CA/login.php
@@ -5,25 +5,20 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
-
-
+  <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>  
-<link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
-
+  <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
 
 <style>
-table, th, td {
-    border: 1px solid black;
-}
+  table, th, td {
+      border: 1px solid black;
+  }
 </style>
 </head>
 
-
 <body>
 
- 
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -31,42 +26,25 @@ table, th, td {
     </div>
     <div>
       <ul class="nav navbar-nav">
-       <li><a href="index.html">Home</a></li>
-        <li><a href="csr.html">CSR</a></li>
-        <li class="active"><a href="sign.html">Sign</a></li>
-       
+        <li><a href="index.php">Home</a></li>
+        <li><a href="csr.php">CSR</a></li>
+        <li class="active"><a href="sign.php">Sign</a></li>
       </ul>
     </div>
   </div>
 </nav>
 
- 
-<span href="#" class="button" id="toggle-login">Login</span>
-
-
-<div id="login">
-  
-<div id="triangle"></div>
-  
-<h1></h1>
-
-<form>
-    <input type="email" placeholder="Email" />
-    
-<input type="password" placeholder="Password" />
-    
-<input type="submit" value="Log in" />
-  
-</form>
-
-
-
+<div id="login">   
+  <h1>Please Login!</h1>
+    <form>
+      <input type="email" placeholder="Email" />        
+      <input type="password" placeholder="Password" />          
+      <input type="submit" value="Log in" />      
+    </form>
 </div>
 
-  <script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
-
-  <script src="js/index.js"></script>
-
+<script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
+<script src="js/index.js"></script>
 </body>
 
 </html>
