b1cf5df25b1deec79c14a63c4f63e2d4598ea5fe
agus4171 <agus.4171.wirawan@gmail.com>
2015-05-06 05:44:06 +0700

    tambahan self signing dan create CA

--- CA/ca-signed.php
+++ CA/ca-signed.php
@@ -2,6 +2,8 @@
 include('File/X509.php');
 include('Crypt/RSA.php');
 
+#DEFINE("OPEN_SSL_CONF_PATH", "C:/xampp/php/extras/openssl/openssl.cnf");
+
 // create private key for CA cert
 $CAPrivKey = new Crypt_RSA();
 extract($CAPrivKey->createKey());
@@ -11,22 +13,16 @@ $pubKey = new Crypt_RSA();
 $pubKey->loadKey($publickey);
 $pubKey->setPublicKey();
 
-echo "the private key for the CA cert (can be discarded):\r\n\r\n";
-echo $privatekey;
-echo "\r\n\r\n";
-
-
-
-
-
-
+#echo "the private key for the CA cert (can be discarded):\r\n\r\n";
+#echo $privatekey;
+#echo "\r\n\r\n";
 
 // create a self-signed cert that'll serve as the CA
 $subject = new File_X509();
 $subject->setDNProp('id-at-countryName', 'ID');
 $subject->setDNProp('id-at-stateOrProvinceName', 'East Java');
 $subject->setDNProp('id-at-localityName', 'Surabaya');
-$subject->setDNProp('id-at-organizationName', 'sampi ningkang CA');
+$subject->setDNProp('id-at-organizationName', 'sampi-ningkang CA');
 $subject->setDNProp('id-at-organizationalUnitName', 'sampi ningkang signer');
 $subject->setDNProp('id-at-commonName', 'www.sampi-ningkang.com');
 $subject->setDNProp('id-at-emailAddress', 'mail@sampi-ningkang.com');
@@ -42,16 +38,25 @@ $x509->setStartDate('-1 month');
 $x509->setEndDate('+1 year');
 
 $result = $x509->sign($issuer, $subject);
-echo "the CA cert to be imported into the browser is as follows:\r\n\r\n";
-echo $x509->saveX509($result);
-echo "\r\n\r\n";
+#echo "the CA cert to be imported into the browser is as follows:\r\n\r\n";
+#echo $x509->saveX509($result);
+#echo "\r\n\r\n";
+$filename = '../simple/privatekey.cert';
+$filename1 = '../simple/root.cert';
+$filename2 = '../simple/publickey.cert';
 
+$myfile = fopen($filename,"w") or die("Unable to open file!");
+fwrite($myfile, $CAPrivKey);
 
+$myfile1 = fopen($filename1,"w") or die("Unable to open file!");
+fwrite($myfile1, $x509->saveX509($result));
 
+$myfile2 = fopen($filename2,"w") or die("Unable to open file!");
+fwrite($myfile2, $pubKey);
 
-
-
-
+fclose($myfile);
+fclose($myfile1);
+fclose($myfile2);
 
 // create private key / x.509 cert for stunnel / website
 $privKey = new Crypt_RSA();
@@ -74,8 +79,8 @@ $issuer->setDN($CASubject);
 $x509 = new File_X509();
 $result = $x509->sign($issuer, $subject);
 echo "the stunnel.pem contents are as follows:\r\n\r\n";
-echo $privKey->getPrivateKey();
+#echo $privKey->getPrivateKey();
 echo "\r\n";
-echo $x509->saveX509($result);
+#echo $x509->saveX509($result);
 echo "\r\n";
 ?>
--- CA/sign.php
+++ CA/sign.php
@@ -6,19 +6,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>  
-  <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+<link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+
 
 <style>
-  table, th, td {
-      border: 1px solid black;
-  }
+table, th, td {
+    border: 1px solid black;
+}
 </style>
 </head>
 
+
 <body>
 
+ 
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -26,26 +31,45 @@
     </div>
     <div>
       <ul class="nav navbar-nav">
-          <li ><a href="#">Home</a></li>
-          <li><a href="create-csr.php">CSR</a></li>
-          <li><a href="signing-ca.php">Sign</a></li>
-          <li class="active"><a href="login.php">Login</a></li> 
+        <li ><a href="#">Home</a></li>
+        <li><a href="create-csr.php">CSR</a></li>
+        <li><a href="signing-ca.php">Sign</a></li>
+        <li class="active"><a href="login.php">Login</a></li> 
+       
       </ul>
     </div>
   </div>
 </nav>
 
-<div id="login">   
-  <h1>Please Login!</h1>
-    <form>
-      <input type="email" placeholder="Email" />        
-      <input type="password" placeholder="Password" />          
-      <input type="submit" value="Log in" />      
-    </form>
+ 
+<span href="#" class="button" id="toggle-login">Sign</span>
+
+
+<div id="login">
+  
+<div id="triangle"></div>
+  
+<h1></h1>
+
+<form>
+
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Link to Certificate</th>
+  </tr>
+  <tr>
+    <td>Agus Corporation</td>
+    <td></td>
+  </tr>
+</table> </form>
+
 </div>
 
-<script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
-<script src="js/index.js"></script>
+  <script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
+
+  <script src="js/index.js"></script>
+
 </body>
 
 </html>
