2a6a17ddd15edabfa8716ca09366e66e7a811731
sdarmaputra <surya.igede@gmail.com>
2015-05-07 02:20:51 +0000

    styling

---
+++ CA/navbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Certificate Authority</a>
+      </div>
+      <div>
+        <ul class="nav navbar-nav">
+        	<li class="<?php if ($page == 'home') echo 'active'; ?>"><a href="#">Home</a></li>
+	        <li class="<?php if ($page == 'csr') echo 'active'; ?>"><a href="create-csr.php">CSR</a></li>
+	        <li class="<?php if ($page == 'sign') echo 'active'; ?>"><a href="sign_csr.php">Sign</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><i>Welcome, <?php echo $username; ?></i></a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
